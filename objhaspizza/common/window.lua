7d203a3f3b78d4094f4a55a1fe99e2a72353bfe0
tsmckinney <tsmckinney10@gmail.com>
2022-10-09 16:38:30 -0400

    Window's been discovering himself a bit lately
    He also figured out how to close and open. No idea why.

--- objhaspizza/common/window.lua
+++ objhaspizza/common/window.lua
@@ -2,6 +2,22 @@ Window = Object:extend()
 
 function Window:new()
     --Window Defaults
+    self.isWindowOpen = true
     love.window.setMode(0,0,{fullscreen = true})
     love.window.setTitle("Object_Has_App")
+end
+
+function Window:setMode(w,h,s)
+    if self.isWindowOpen == true then
+        love.window.updateMode(w,h,s)
+    else
+        love.window.setMode(w,h,s)
+    end
+end
+function Window:setTitle(t)
+    love.window.setTitle(t)
+end
+function Window:close()
+    love.window.close()
+    self.isWindowOpen = false
 end
