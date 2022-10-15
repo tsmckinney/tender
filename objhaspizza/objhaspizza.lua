fb9b079a044fa31d79fcc273f06cc54ebbfa8296
tsmckinney <tsmckinney10@gmail.com>
2022-10-15 14:27:05 -0400

    Okay, so he didn't actually say anything, but now he's talking!

--- objhaspizza/objhaspizza.lua
+++ objhaspizza/objhaspizza.lua
@@ -1,8 +1,10 @@
 ObjHasPizza = Object:extend()
 --I know, it's a long class name, but it's better to be safe than sorry.
 require("object-has-pizza.objhaspizza.common.window")
+require("object-has-pizza.objhaspizza.common.logging")
 function ObjHasPizza:new()
     self.window = Window()
+    self.log = Logging()
 end
 function ObjHasPizza:update(dt)
     
