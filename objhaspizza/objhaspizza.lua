b32103421ae34aa1e7cfc7a86a434999ece532a4
tsmckinney <tsmckinney10@gmail.com>
2022-11-05 10:39:03 -0400

    Added a grid for debugging
    Just to make sure Pizza knows where he's placing things.

--- objhaspizza/objhaspizza.lua
+++ objhaspizza/objhaspizza.lua
@@ -3,16 +3,18 @@ ObjHasPizza = Object:extend()
 require("object-has-pizza.objhaspizza.common.window")
 require("object-has-pizza.objhaspizza.common.logging")
 require("object-has-pizza.objhaspizza.graphics.image")
+require("object-has-pizza.objhaspizza.graphics.debugGrid")
 function ObjHasPizza:new()
     self.window = OHWindow()
     self.log = OHLogging()
     self.imgload = OHImage()
+    self.debugGrid = OHDebugGrid(0.5)
 end
 function ObjHasPizza:update(dt)
     
 end
 function ObjHasPizza:draw()
-    
+    self.debugGrid:draw()
 end
 function ObjHasPizza:quit()
     love.event.quit()
